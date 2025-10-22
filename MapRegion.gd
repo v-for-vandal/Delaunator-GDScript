f45e61156079de96d81085a915abbc01905962d8
Artem Serebriyskiy <v.for.vandal@gmail.com>
2025-10-22 20:34:46 +0300

    fix demo for Delaunator

---
+++ MapRegion.gd
@@ -0,0 +1,25 @@
+extends Area2D
+class_name MapRegion
+
+signal region_selected
+
+var shape : PackedVector2Array: set = set_shape
+
+@onready var _poly := $Polygon2D
+@onready var _coll := $CollisionPolygon2D
+
+func set_shape(new_shape: PackedVector2Array):
+	_poly.set_polygon(new_shape)
+	_poly.color = Color(randf(), randf(), randf(), 0.6)
+	_coll.set_polygon(new_shape)
+	shape = new_shape
+
+func _on_MapRegion_mouse_entered():
+	_poly.color.a = 1
+
+func _on_MapRegion_mouse_exited():
+	_poly.color.a = 0.6
+
+func _on_MapRegion_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		region_selected.emit()
